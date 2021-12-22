d691654b1e2729147cd51bfe3280d7dc27a45fb9
tina41218 <tina41218@gmail.com>
2021-12-22 10:52:35 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bookkeep
+{
+    static class Anaccount
+    {
+        public static string Time(string time)
+        {
+            time = Console.ReadLine();
+            return time;
+        }
+        public static string Money(int money)
+        {
+            money = int.Parse(Console.ReadLine());
+            return money;
+        }
+        public static string Classification(string classification)
+        {
+            classification = Console.ReadLine();
+            return classification;
+        }
+        public static string Paymentmethod(string paymentmethod)
+        {
+            paymentmethod = Console.ReadLine();
+            return paymentmethod;
+        }
+        public static string Remark(string Remark)
+        {
+            Remark = Console.ReadLine();
+            return Remark;
+        }
+    }
+}
+
